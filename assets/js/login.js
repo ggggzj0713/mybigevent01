5e788364907e6a1032cdeb76c4a51bd554e51c34
gggzj <1346072714@qq.com>
2020-10-07 19:39:12 +0800

    注册和登录功能完成

---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+$(function() {
+    // 点击去注册页面
+    $('#link_reg').on('click', function() {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    });
+    // 点击去往登录页面
+    $('#link_login').on('click', function() {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    });
+
+
+    // 自定义校验规则
+    var form = layui.form
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function(value) {
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次输入的密码不一致'
+            }
+        }
+    });
+    var layer = layui.layer;
+    // 账号注册
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/api/reguser',
+            data: {
+                username: $('.reg-box [name=username]').val(),
+                password: $('.reg-box [name=password]').val(),
+            },
+            success: function(res) {
+                if (res.status != 0) {
+                    return console.log(res.message);
+                }
+                layer.msg('注册成功');
+                $('#link_login').click();
+                $('#form_reg')[0].reset();
+            }
+        })
+
+    });
+    $('#form_login').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status != 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('登录成功');
+                localStorage.setItem('token', res.token);
+                location.href = '/index.html'
+            }
+        })
+    })
+
+
+})
